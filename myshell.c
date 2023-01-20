3cdaea8971556c0335d7d2dfe24d2388f4ba5c6a
Yash <chandakyu20.comp@coep.ac.in>
2023-01-20 17:03:51 +0530

    copy

--- myshell.c
+++ myshell.c
@@ -3,94 +3,140 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-int myexec(char *argv[], char *path[]) {
+int myexec(char *argv[], char *path[], int or, int ir) {
 	int pid = fork();
-	if(pid == 0) {
+	if (pid == 0) { // agar child ise run karta to ye kam karao 
+		if(or!=-1) {
+			close(1);
+			int fd = open(argv[or+1], O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR);
+			argv[or] = NULL;
+		}
+		if(ir!=-1) {
+			close(0);
+			int fd = open(argv[ir+1], O_RDONLY, S_IRUSR);
+			argv[ir] = NULL;
+		}
 		
-		int i=0;
-		while(i<4) {
-			char pa[50]="";
+		int i = 0;
+		while (i < 5)
+		{
+			char pa[50] = "";
 			strcpy(pa, path[i]);
 			strcpy(pa, strcat(pa, argv[0]));
-			
-			if(execv(pa, argv) == -1) {
+
+			if (execv(pa, argv) == -1) {
 				i++;
 			}
-			else {	
+			else {
 				break;
 			}
 		}
 		printf("command not found\n");
 	}
-	else {
+	else { // parent to wait karo child ke liye
 		wait(0);
-	}	
+	}
 }
 
 int main() {
-	// int pid;
-	char *PATH[10] = {"", "/usr/bin/", "/usr/sbin/", "/usr/games/", "/snap/bin/"};
-	int pi=4;
+	char *PATH[10] = {"/usr/bin/", "/usr/sbin/", "/usr/games/", "/snap/bin/"};
+	int pi = 0;
 	char str[1000];
 	char str2[1000];
-		
-	while(1) {
+
+	int prompt = 0;
+	char *PS1 = (char *)malloc(sizeof(char)*50);
+
+	while (1) {
 
 		char cwd[50];
 		getcwd(cwd, sizeof(cwd));
-		printf("\n__yash__%s__:\n$", cwd);
-		
-		fgets(str, 1000, stdin);
+		if(prompt == 0) {
+			printf("\n__yash__%s__:\n$", cwd);
+		}
+		else {
+			printf("%s",PS1);
+		}
+		char *ctrld = fgets(str, 1000, stdin);
+
+		if(!ctrld || !strcmp(str, "exit\n")) {
+			return 0;
+		}
+
 		strcpy(str2, str);
 
-		PATH[0] = (char *)malloc(sizeof(char)*50);
-		strcat(cwd, "/");
-		strcpy(PATH[0], cwd);
-
-		if(!strcmp(str, "exit\n")) return 0;
 		char *argv[10];
-		
+
 		char *first = strtok(str, "=");
 		if(!strcmp(first, "PATH")) {
 			char *last = strtok(NULL, "=");
 			last = strtok(last, "\n");
-			PATH[pi] = (char *)malloc(sizeof(char)*50);
-			strcpy(PATH[pi], last);
+			last = strtok(last, ":");
+			while(last) {
+				PATH[pi] = (char *)malloc(sizeof(char) * 50);
+				strcpy(PATH[pi], last);
+				printf("%s ", PATH[pi]);
+				pi++;
+				last = strtok(NULL, ":");
+			}
+			continue;
+		}
+		else if(!strcmp(first, "PS1")) {
+			char *last = strtok(NULL, "=");
+			last = strtok(last, "\n");
+			if(!strcmp(last, "\"\\w$\"")) {
+				prompt=0;
+			}
+			else {
+				prompt = 1;
+				strcpy(PS1, last);
+			}
 		}
 
-		// printf("%s\ndf", str);
-
 		first = strtok(str, " ");
-		if(!strcmp(first, "cd")) {
+		if (!strcmp(first, "cd")) {
 			char *last = strtok(NULL, " ");
 			last = strtok(last, "\n");
 			chdir(last);
 			continue;
 		}
-		
-		// printf("%s\ndf", str);
 
-		for(int i=0; i<10; i++) {
-			argv[i] = (char *)malloc(sizeof(char)*100);	
+		strcpy(str, str2);
+
+
+
+		for (int i = 0; i < 10; i++) {
+			argv[i] = (char *)malloc(sizeof(char) * 100);
 		}
-		
-		int i=0, j=0, k=0;
-		while(1) {
-			while(str2[i] == ' ') {
+
+		int i = 0, j = 0, k = 0, or=-1, ir=-1;
+		while (1) {
+			while (str2[i] == ' ') {
 				i++;
 			}
-			while(str2[i] != ' ' && str2[i] != '\n') {
+			while (str2[i] != ' ' && str2[i] != '\n') {
+				if(str2[i]=='>') {
+					or = j;
+				}
+				else if(str2[i]=='<') {
+					ir = j;
+				}
 				argv[j][k] = str2[i];
 				i++;
 				k++;
 			}
 			argv[j][k] = '\0';
 			j++;
-			if(str2[i]=='\n') break;
-			k=0;
+			if (str2[i] == '\n')
+				break;
+			k = 0;
 		}
 		argv[j] = NULL;
-		myexec(argv, PATH);
+
+		myexec(argv, PATH, or, ir);
 	}
 }
--- tp.c
+++ tp.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
+#include<string.h>
 
 int main() {
     printf("a.out me a.out\n");
+    char buf[20] = "yash";
+    char hh[10] = "hoho";
+    strcpy(buf, hh);
+    printf("%s", buf);
 }
